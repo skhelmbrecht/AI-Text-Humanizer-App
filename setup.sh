74a6ff50daa768c8a38349ae847becb20d894859
DADA NANJESHA <dadananjesha.rymec@gmail.com>
2025-02-08 04:29:11 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@
 echo "Installing pip dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install spacy sentence-transformers nltk
 
 echo "Installing spaCy model..."
 python -m spacy download en_core_web_sm
+python -m nltk.downloader all
+
 
 echo "Downloading NLTK data..."
 python -c "import nltk; nltk.download('punkt_tab', quiet=True); nltk.download('punkt', quiet=True); nltk.download('wordnet', quiet=True); nltk.download('averaged_perceptron_tagger', quiet=True)"
